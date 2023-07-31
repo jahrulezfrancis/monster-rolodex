ea53461f6c63f26a33ee3b99fcf9c4dca30311ce
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-07-31 15:32:14 +0100

    Implement search logic and redering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import MonsterCardComp from './Components/Monsters/Monster.component';
+import Picture from "./Media/J5P.png"
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => { setMonsters(users) })
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value.toLocaleLowerCase())
+  }
   return (
     <div className="App">
-      
-    </div>
+      <input onChange={handleChange} style={{ height: '35px' }} type='search' placeholder='Search Monsters' />
+      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", }}>
+        {
+          monsters.map((monst) => {
+            return (
+              <div style={{ display: "inline" }} key={monst.id}>
+                {(monst.name.toLocaleLowerCase().includes(searchTerm)) ?
+                  <div style={{ display: "flex" }}>
+                    <MonsterCardComp monsterQuote={monst.company.catchPhrase} monsterEmail={monst.email} monsterImage={Picture} monsterName={monst.name} />
+                  </div>
+                  :
+                  ""
+                }
+              </div>
+            )
+          })
+        }
+      </div>
+
+    </div >
   );
 }
 
 export default App;
+
+const styles = {
+  display: "flex",
+  flexDirection: "row"
+}
