51d61b50c6951fa75dfd5d9c2c3dfe1190ac1f2c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-01 12:04:18 +0100

    Update image source

--- src/Components/Monsters/Monster.component.jsx
+++ src/Components/Monsters/Monster.component.jsx
@@ -2,19 +2,23 @@ import './monster.css'
 
 
 export default function MonsterCardComp(props) {
-    const { monsterImage, monsterName, monsterQuote, monsterEmail, monsters, searchTerm } = props;
-    return (monsters.map((monster) => {
-        return (
-            (monster.name.toLocaleLowerCase().includes(searchTerm)) ?
-                <div className="monster-card-container">
-                    < img src={monsterImage} alt="monster" />
-                    <h2>{monster.name}</h2>
-                    <h3>{monster.email}</h3>
-                    <p>{monster.company.catchPhrase}</p>
-                </div>
-
-                : ""
-        )
-    })
+    const { monsters, searchTerm } = props;
+    // style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", width: '100%' }}
+    return (
+        <div className='card-list-container'>
+            {monsters.map((monster) => {
+                return (
+                    (monster.name.toLocaleLowerCase().includes(searchTerm)) ?
+                        <div className="monster-card-container">
+                            <img src={`http://robohash.org/${monster.id}?set=set2&size=180x180`} alt="monster" />
+                            <h2>{monster.name}</h2>
+                            <h3>{monster.email}</h3>
+                            <p>{monster.company.catchPhrase}</p>
+                        </div>
+                        :
+                        null
+                )
+            })}
+        </div>
     )
 }
